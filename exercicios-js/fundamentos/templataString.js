6bd1a68bcf83e96e501ba13ad76e7a161bb54704
IsabellaFSilva <isabellafs023@gmail.com>
2019-11-18 17:49:49 -0200

    Seção 3, aula 21

---
+++ exercicios-js/fundamentos/templataString.js
@@ -0,0 +1,6 @@
+const nome = 'Isabella'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá 
+    ${nome}!`
+console.log(concatenacao, template)
---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3 //Variavel global
+let b = 4 //Variavel local
+
+var a =30
+b = 40
+
+console.log(a ,b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+//Contante
+const c = 5
+//c = 50
+console.log(c)
